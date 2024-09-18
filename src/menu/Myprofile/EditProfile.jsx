695b1eeb62b55d1b223d720035dcb906ee7ddf6c
Rohan Gope <rg2512@it.jgec.ac.in>
2024-09-18 23:14:21 +0530

    first commit

---
+++ src/menu/Myprofile/EditProfile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import BackBtn from '../../components/BackBtn';
+import NavMenu from '../../components/NavMenu';
+
+const EditProfile = () => {
+  return (
+    <View>
+      <NavMenu />
+      <View>
+        {/* <BackBtn />
+        <Text>Fare Card</Text> */}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditProfile;
